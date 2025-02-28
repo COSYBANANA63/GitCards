932a361c06ad0abf31d3c69e560e93683f6208c9
COSYBANANA <tobyfemi55@gmail.com>
2025-02-28 19:29:25 +0100

    Refactor CSS styles for menu buttons and dropdown; enhance theme support and add hover effects

--- www/js/index.js
+++ www/js/index.js
@@ -401,25 +401,25 @@ function createSavedCard(profile) {
                 </svg>
             </button>
             <div class="card-menu-dropdown hidden">
-                <button onclick="event.stopPropagation(); shareGitHubProfile('${profile.username}')">
+                <button class="share-action" onclick="event.stopPropagation(); shareGitHubProfile('${profile.username}')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="action-icon">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
                     </svg>
                     Share
                 </button>
-                <button onclick="event.stopPropagation(); deleteProfile(${profile.id})">
+                <button class="delete-action" onclick="event.stopPropagation(); deleteProfile(${profile.id})">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="action-icon">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
                     Delete
                 </button>
-                <button onclick="event.stopPropagation(); showMessageDialog(${profile.id}, '${profile.name}')">
+                <button class="message-action" onclick="event.stopPropagation(); showMessageDialog(${profile.id}, '${profile.name}')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="action-icon">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
                     </svg>
                     Messages
                 </button>
-                <button onclick="event.stopPropagation(); window.open('https://github.com/${profile.username}', '_blank')">
+                <button class="github-action" onclick="event.stopPropagation(); window.open('https://github.com/${profile.username}', '_blank')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="action-icon">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                     </svg>
